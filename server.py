4412b43422d4d07c21453ee029d20e4aaa0e6f7a
Isaac <chungisaac1217@gmail.com>
2020-06-05 21:58:11 -0400

    Flask app fix for end of week 2

--- server.py
+++ server.py
@@ -1,34 +1,61 @@
 from flask import Flask, render_template, request, session, make_response
-from processing import process_data
+from image_search import *
+import os
 
 # Create the application object
 app = Flask(__name__)
+
+# load index and model
+input_dataset = ImageDataset('notebooks/data')
+bs = 100
+image_loader = torch.utils.data.DataLoader(input_dataset, batch_size=bs)
+model = load_pretrained_model()
+
+pd_files = input_dataset.get_file_df()
+annoy_path = 'notebooks/annoy_idx.annoy'
+if os.path.exists(annoy_path):
+	annoy_idx_loaded = AnnoyIndex(512)
+	annoy_idx_loaded.load(annoy_path)
+
 
 @app.route('/')
 def index():
 	title_text = 'NextPick - by Isaac Chung'
 	return render_template('index.html', title=title_text, username='chooch')
 
-# @app.route('/output', methods=['POST'])
-# def output():
-#    	# Pull input
-# 	some_input = request.args.get('test')
-#
-# 	# Case if empty
-# 	if some_input == "":
-# 		return render_template("templates/index.html",
-# 							   my_input=some_input,
-# 							   my_form_result="Empty")
-# 	else:
-# 		some_output = "yeay!"
-# 	some_number = 3
-# 	some_image = "static/assets/img/ipad.jpg"
-# 	return render_template("index.html",
-# 						   my_input=some_input,
-# 						   my_output=some_output,
-# 						   my_number=some_number,
-# 						   my_img_name=some_image,
-# 						   my_form_result="NotEmpty")
+@app.route('/output')
+def output():
+	title_text = 'NextPick - by Isaac Chung'
+	selection = request.args.get("selection")
+
+	# Case if empty
+	if selection == "ski":
+		print("..ski tag")
+		test_img = 'notebooks/ski-test-img.png'
+		searches = evalTestImage(test_img, model, annoy_idx_loaded)
+		df = create_df_for_map_plot(searches, pd_files)
+		print(df['cos_diff'])
+
+		return render_template("index.html", title=title_text,flag="1",
+							   sel_input=selection,	sel_result="skiing",
+							   cos1='%.3f'%df['cos_diff'][0]
+							   )
+	elif selection == "war_mem":
+		print("..war_mem tag")
+		return render_template("index.html",
+							   title=title_text,
+							   flag="1",
+							   sel_input=selection,
+							   sel_result="memorialzzzz")
+	else:
+		print("..whatever")
+		some_output = "yeay!"
+	return render_template("index.html",
+						   title=title_text,
+						   flag="0",
+						   some_output=some_output,
+						   sel_input=selection,
+						   sel_form_result="Empty")
 
 
 # start the server with the 'run()' method
