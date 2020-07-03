1fa9afcacb5a3c9f899ecae0e07005e8da9a028d
Isaac Chung <48971969+isaac-chung@users.noreply.github.com>
2020-07-02 22:18:38 -0400

    resolve merge conflicts for server.py

--- NextPick-app/NextPick/server.py
+++ NextPick-app/NextPick/server.py
@@ -7,7 +7,6 @@ import os
 import torch
 from base64 import b64encode
 import pickle
-<<<<<<< HEAD:NextPick-app/NextPick/server.py
 from NextPick import app
 
 APP_PATH = '/home/ubuntu/application'
@@ -21,8 +20,6 @@ TOP_N = 20
 ANNOY_PATH = '%s/NextPick/NextPick/annoy_idx.annoy' %APP_PATH
 ANNOY_METRIC = 'angular'
 RESNET18_FEAT = 512
-=======
->>>>>>> master:server.py
 
 
 # Create the application object
@@ -33,20 +30,12 @@ app.secret_key = 'random'
 input_dataset = ImageDataset(DATA_FOLDER)
 image_loader = torch.utils.data.DataLoader(input_dataset, batch_size=BATCH)
 model, model_full = load_pretrained_model()
-<<<<<<< HEAD:NextPick-app/NextPick/server.py
 fname_df = '%s/NextPick/ski_pd_files.pkl' % APP_PATH
 with open(fname_df, 'rb') as f:
 	pd_files = pickle.load(f)
 	f.close()
 # pd_files = input_dataset.get_file_df()
 annoy_path = ANNOY_PATH
-=======
-
-with open('NextPick/pd_files.pkl','rb') as f:
-    pd_files = pickle.load(f)
-    f.close()
-annoy_path = cfg.ANNOY_PATH
->>>>>>> master:server.py
 if os.path.exists(annoy_path):
 	annoy_idx_loaded = AnnoyIndex(RESNET18_FEAT, metric=ANNOY_METRIC)
 	annoy_idx_loaded.load(annoy_path)
@@ -109,5 +98,3 @@ def upload_img():
 @app.route('/<path:filename>')
 def download_file(filename):
 	return send_from_directory(DATA_FOLDER, filename, as_attachment=True)
-
-
