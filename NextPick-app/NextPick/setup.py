f46d6cdfd8235d4d7fe1a69239886da9d9b0bd30
Isaac <chungisaac1217@gmail.com>
2020-09-15 19:13:20 -0400

    Added details in setup file.

--- NextPick-app/NextPick/setup.py
+++ NextPick-app/NextPick/setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='nextpick',
+    name='NextPick',
     version='0.1.0',
     description='Project for Insight DS TO 2020B.',
     author='isaac',
+    author_email='chungisaac1217@gmail.com',
+    packages=['NextPick'],
     license='MIT',
 )
